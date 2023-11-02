93dee01d284b5ea465b030776a191129b10a4278
Jason Perez <jasonper@andrew.cmu.edu>
2023-11-02 01:27:09 -0400

    Initial commit

---
+++ SkateSpotsMap/SkateSpotsMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkateSpotsMapApp.swift
+//  SkateSpotsMap
+//
+//  Created by Jason on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SkateSpotsMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
