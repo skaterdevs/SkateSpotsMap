c01f1a9b59eb83e4d5c638a11bdc3d43fbf3859a
Jason Perez <jasonper@andrew.cmu.edu>
2023-11-03 20:13:31 -0400

    Updated Amplify settings

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,19 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "skatespotsmapad3fd4ba": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "skaterstorage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
