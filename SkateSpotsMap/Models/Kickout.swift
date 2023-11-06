07123794cace7d90b09fe8c8ad68b161ab8d1432
Jason Perez <jasonper@andrew.cmu.edu>
2023-11-06 17:38:11 -0500

    Created SkateSpot, Feature, and Kickout models and SkateSpotRepository

---
+++ SkateSpotsMap/Models/Kickout.swift
@@ -0,0 +1,14 @@
+//
+//  Kickout.swift
+//  SkateSpotsMap
+//
+//  Created by Jason on 11/6/23.
+//
+
+import Foundation
+
+enum Kickout: String {
+    case low = "Low"
+    case medium = "Medium"
+    case high = "High"
+}
