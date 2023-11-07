1a5bf98c6685d1dc8f45562cfa605d526fbfc3e5
idolish7 <paradice476@gmail.com>
2023-11-07 09:49:30 -0500

    Created view files

---
+++ SkateSpotsMap/Views/SkateSpots/NewSkateSpotView.swift
@@ -0,0 +1,18 @@
+//
+//  NewSkateSpotView.swift
+//  SkateSpotsMap
+//
+//  Created by Cameron Wu on 11/7/23.
+//
+
+import SwiftUI
+
+struct NewSkateSpotView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewSkateSpotView()
+}
---
+++ SkateSpotsMap/Views/SkateSpots/SkateSpotRowView.swift
@@ -0,0 +1,18 @@
+//
+//  SkateSpotRowView.swift
+//  SkateSpotsMap
+//
+//  Created by Cameron Wu on 11/7/23.
+//
+
+import SwiftUI
+
+struct SkateSpotRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SkateSpotRowView()
+}
