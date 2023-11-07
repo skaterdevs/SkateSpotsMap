f1511866d61d4b30b85f2b38c6469f997e817943
idolish7 <paradice476@gmail.com>
2023-11-07 12:05:22 -0500

    Created ViewModel files

---
+++ SkateSpotsMap/ViewModels/ReviewViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ReviewViewModel.swift
+//  SkateSpotsMap
+//
+//  Created by Cameron Wu on 11/7/23.
+//
+
+import Foundation
---
+++ SkateSpotsMap/ViewModels/UserViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserViewModel.swift
+//  SkateSpotsMap
+//
+//  Created by Cameron Wu on 11/7/23.
+//
+
+import Foundation
