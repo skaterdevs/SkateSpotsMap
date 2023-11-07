e938e1e33a68b56f4319121ac540c5db82225649
leinard0 <darriola@andrew.cmu.edu>
2023-11-07 00:55:37 -0500

    Added static example to SkateSpot model for use in SwiftUI

--- SkateSpotsMap/Models/SkateSpot.swift
+++ SkateSpotsMap/Models/SkateSpot.swift
@@ -45,4 +45,17 @@ struct SkateSpot: Identifiable, Comparable, Codable {
   static func <(lhs: SkateSpot, rhs: SkateSpot) -> Bool {
     return lhs.rating_avg < rhs.rating_avg
   }
+  
+  // MARK: Example for SwiftUI
+  static let example = SkateSpot(
+    id: "dsalfjdlfjcl",
+    name: "Schenely Park",
+    location: GeoPoint(latitude: 3.2, longitude: 4.1),
+    photos: ["basic.landscape"],
+    features: [Feature.banks.rawValue],
+    overall_kickout: Kickout.low.rawValue,
+    rating_sum: 4,
+    rating_avg: 4.0,
+    tags: [Tag.CityArea.rawValue],
+    reviews: [Review.example])
 }
