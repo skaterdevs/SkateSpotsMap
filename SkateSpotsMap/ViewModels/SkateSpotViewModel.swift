264b29613c984b5da8591e3325fb341bfd07afd8
leinard0 <darriola@andrew.cmu.edu>
2023-11-09 11:30:25 -0500

    Created SkateSpotViewModel with function for redirecting application to apple maps with location from skateSpot preloaded

--- SkateSpotsMap/ViewModels/SkateSpotViewModel.swift
+++ SkateSpotsMap/ViewModels/SkateSpotViewModel.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import SwiftUI
+
+class SkateSpotViewModel: ObservableObject {
+    private var skateSpotRepository = SkateSpotRepository()
+    @Published var skateSpots: [SkateSpot]
+
+    init() {
+        self.skateSpots = skateSpotRepository.skate_spots
+    }
+
+    func add(skateSpot: SkateSpot) {
+        skateSpotRepository.create(skateSpot)
+    }
+  
+    func destroy(skateSpot: SkateSpot) {
+        skateSpotRepository.delete(skateSpot)
+    }
+    
+    func appleMapsRedirect(skateSpot: SkateSpot) {
+        let lat: Double = skateSpot.location.latitude
+        let long: Double = skateSpot.location.longitude
+        let mapsURL = URL(string: "maps://?dirflg=w&saddr=&daddr=\(lat),\(long)")
+        UIApplication.shared.open(mapsURL!,options: [:],
+        completionHandler: nil)
+    }
+}
