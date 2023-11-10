55346cf2f94675cff34a7be32ddeaa7faf2e5c4e
Jason Perez <jasonper@andrew.cmu.edu>
2023-11-10 06:20:56 -0500

    Added Map and Photo Picker views to NewSkateSpotView; Fixed indentation on other SkateSpot views;

--- SkateSpotsMap/Views/SkateSpot/SkateSpotDetailView.swift
+++ SkateSpotsMap/Views/SkateSpot/SkateSpotDetailView.swift
@@ -14,63 +14,63 @@ struct SkateSpotDetailView: View {
     var skateSpot: SkateSpot?
   
     var body: some View {
-      VStack{
-        HStack{
-          Text(skateSpot!.name)
-            .font(.largeTitle)
-            .fontWeight(.bold)
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding()
-          Button{
-            skateSpotViewModel.appleMapsRedirect(skateSpot: skateSpot!)
-            } label: {
-              Image(systemName: "arrow.triangle.turn.up.right.diamond.fill")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 40)
-                .foregroundStyle(.green)
-          }
-          Spacer()
-        }
-        
-        // image carousel
-        ScrollView(.horizontal) {
-          HStack(spacing: 10) {
-            // TODO: make show skateSpot.photos
-            ForEach(images, id: \.self) { image in
-                Image(uiImage: image)
-                    .frame(width: 200, height: 200)
-                    .clipShape(Rectangle())
-                    .padding([.leading, .vertical], 5)
+        VStack{
+            HStack{
+                Text(skateSpot!.name)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                Button{
+                    skateSpotViewModel.appleMapsRedirect(skateSpot: skateSpot!)
+                } label: {
+                    Image(systemName: "arrow.triangle.turn.up.right.diamond.fill")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 40)
+                        .foregroundStyle(.green)
+                }
+                Spacer()
             }
-          }
-        }.onAppear() { downloadImages(image_keys: skateSpot!.photos) }
-        
-        Divider().frame(height: 15)
-        
-        // overview and reviews
-        HStack {
-          Spacer()
-          NavigationLink(
-            destination: SkateSpotOverviewView(skateSpot: skateSpot!),
-            label: {
-              Text("Overview")
-            })
-          Spacer()
-          NavigationLink(
-            destination: SkateSpotReviewsView(skateSpot: skateSpot!),
-            label: {
-              Text("Reviews")
-            })
-          Spacer()
+            
+            // image carousel
+            ScrollView(.horizontal) {
+                HStack(spacing: 10) {
+                    // TODO: make show skateSpot.photos
+                    ForEach(images, id: \.self) { image in
+                        Image(uiImage: image)
+                            .frame(width: 200, height: 200)
+                            .clipShape(Rectangle())
+                            .padding([.leading, .vertical], 5)
+                    }
+                }
+            }.onAppear() { downloadImages(image_keys: skateSpot!.photos) }
+            
+            Divider().frame(height: 15)
+            
+            // overview and reviews
+            HStack {
+                Spacer()
+                NavigationLink(
+                    destination: SkateSpotOverviewView(skateSpot: skateSpot!),
+                    label: {
+                        Text("Overview")
+                    })
+                Spacer()
+                NavigationLink(
+                    destination: SkateSpotReviewsView(skateSpot: skateSpot!),
+                    label: {
+                        Text("Reviews")
+                    })
+                Spacer()
+            }
+            
+            Divider().frame(height: 15)
+            
+            SkateSpotOverviewView(skateSpot: skateSpot!)
+            
+            Spacer()
         }
-        
-        Divider().frame(height: 15)
-        
-        SkateSpotOverviewView(skateSpot: skateSpot!)
-        
-        Spacer()
-      }
     }
     
     func downloadImages(image_keys: [String]) {
@@ -91,9 +91,9 @@ struct SkateSpotDetailView: View {
 }
 
 struct SkateSpotDetailView_Previews: PreviewProvider {
-  static var previews: some View {
-    SkateSpotDetailView(skateSpot: SkateSpot.example)
-  }
+    static var previews: some View {
+        SkateSpotDetailView(skateSpot: SkateSpot.example)
+    }
 }
 
 //#Preview {
--- SkateSpotsMap/Views/SkateSpot/SkateSpotOverviewView.swift
+++ SkateSpotsMap/Views/SkateSpot/SkateSpotOverviewView.swift
@@ -8,82 +8,82 @@
 import SwiftUI
 
 struct SkateSpotOverviewView: View {
-  var skateSpot: SkateSpot
-  @State var distance: String?
-  @StateObject var locationManager = LocationManager()
-  @ObservedObject var skateSpotViewModel = SkateSpotViewModel()
-  
-  var body: some View {
-    VStack {
-      
-      HStack {
-        Spacer()
+    var skateSpot: SkateSpot
+    @State var distance: String?
+    @StateObject var locationManager = LocationManager()
+    @ObservedObject var skateSpotViewModel = SkateSpotViewModel()
+    
+    var body: some View {
         VStack {
-          Text("Distance".uppercased()).fontWeight(.light)
-          Text(distance ?? "Unavailable").fontWeight(.bold)
-        }
-        Spacer()
-        VStack {
-          Text("Rating".uppercased()).fontWeight(.light)
-          Text("\(String(format: "%.2f", skateSpot.rating_avg))").fontWeight(.bold)
-        }
-        Spacer()
-        VStack {
-          Text("Kick-out Factor".uppercased()).fontWeight(.light)
-          Text("\(skateSpot.overall_kickout)").fontWeight(.bold)
-        }
-        Spacer()
-      }
-      
-      Divider().frame(height: 15)
-      
-      VStack {
-        HStack {
-          HStack(spacing: 20) {
-            // TODO: make show skateSpot.photos
-            ForEach(skateSpot.tags, id: \.self) { tag in
-              Text(tag)
-                .padding(10)
-                .overlay(
-                  RoundedRectangle(cornerRadius: 30)
-                    .stroke(Color.black, lineWidth: 1)
-                )
+            
+            HStack {
+                Spacer()
+                VStack {
+                    Text("Distance".uppercased()).fontWeight(.light)
+                    Text(distance ?? "Unavailable").fontWeight(.bold)
+                }
+                Spacer()
+                VStack {
+                    Text("Rating".uppercased()).fontWeight(.light)
+                    Text("\(String(format: "%.2f", skateSpot.rating_avg))").fontWeight(.bold)
+                }
+                Spacer()
+                VStack {
+                    Text("Kick-out Factor".uppercased()).fontWeight(.light)
+                    Text("\(skateSpot.overall_kickout)").fontWeight(.bold)
+                }
+                Spacer()
             }
-          }
+            
+            Divider().frame(height: 15)
+            
+            VStack {
+                HStack {
+                    HStack(spacing: 20) {
+                        // TODO: make show skateSpot.photos
+                        ForEach(skateSpot.tags, id: \.self) { tag in
+                            Text(tag)
+                                .padding(10)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 30)
+                                        .stroke(Color.black, lineWidth: 1)
+                                )
+                        }
+                    }
+                }
+            }
+            
+            Divider().frame(height: 15)
+            
+            // TODO: Features
+            
+            VStack {
+                HStack(spacing: 20) {
+                    ForEach(skateSpot.features, id: \.self) { feature in
+                        Text(feature)
+                    }
+                }
+            }
+            
+        }.task {
+            
+            do {
+                let sLat = locationManager.userLatitude()
+                let sLong = locationManager.userLongitude()
+                let dLat = skateSpot.location.latitude
+                let dLong = skateSpot.location.longitude
+                distance = try await skateSpotViewModel.getDist(sLat: sLat, sLong: sLong, dLat: dLat, dLong: dLong)
+            } catch {
+                distance = nil
+            }
         }
-      }
-      
-      Divider().frame(height: 15)
-      
-      // TODO: Features
-      
-      VStack {
-        HStack(spacing: 20) {
-          ForEach(skateSpot.features, id: \.self) { feature in
-            Text(feature)
-          }
-        }
-      }
-      
-    }.task {
-      
-      do {
-        let sLat = locationManager.userLatitude()
-        let sLong = locationManager.userLongitude()
-        let dLat = skateSpot.location.latitude
-        let dLong = skateSpot.location.longitude
-        distance = try await skateSpotViewModel.getDist(sLat: sLat, sLong: sLong, dLat: dLat, dLong: dLong)
-      } catch {
-        distance = nil
-      }
     }
-  }
 }
 
 struct SkateSpotOverviewView_Previews: PreviewProvider {
-  static var previews: some View {
-    SkateSpotOverviewView(skateSpot: SkateSpot.example)
-  }
+    static var previews: some View {
+        SkateSpotOverviewView(skateSpot: SkateSpot.example)
+    }
 }
 
 //#Preview {
