cbdf46c96db09c42ba70cb9742b34efb2e0f3312
Jason Perez <jasonper@andrew.cmu.edu>
2023-11-10 03:43:31 -0500

    Created GoogleMapViewModel specifically for NewSkateSpotView

---
+++ SkateSpotsMap/ViewModels/AddSpotGoogleMapViewModel.swift
@@ -0,0 +1,58 @@
+//
+//  AddSpotGoogleMapViewModel.swift
+//  SkateSpotsMap
+//
+//  Created by Jason Perez on 11/10/23.
+//
+
+import Foundation
+import GoogleMaps
+import SwiftUI
+
+struct AddSpotGoogleMapViewModel: UIViewControllerRepresentable {
+    @Binding var selectedMarker: GMSMarker?
+    
+    func makeUIViewController(context: Context) -> GoogleMapViewModel {
+        let uiViewModel = GoogleMapViewModel()
+        uiViewModel.mapView.delegate = context.coordinator
+        // Enable user location
+        uiViewModel.mapView.isMyLocationEnabled = true
+        // Allow users to recenter
+        uiViewModel.mapView.settings.myLocationButton = true
+        return uiViewModel
+    }
+    
+    func updateUIViewController(_ uiViewModel: GoogleMapViewModel, context: Context) {
+        // Default camera position to user location
+        let userLat = uiViewModel.mapView.myLocation?.coordinate.latitude
+        let userLong = uiViewModel.mapView.myLocation?.coordinate.longitude
+        uiViewModel.mapView.camera = GMSCameraPosition.camera(withLatitude: userLat ?? 0,
+                                                              longitude: userLong ?? 0,
+                                                              zoom: 15)
+    }
+    
+    func makeCoordinator() -> MapViewCoordinator {
+        return MapViewCoordinator(self)
+    }
+    
+    final class MapViewCoordinator: NSObject, GMSMapViewDelegate {
+        var addSpotGoogleMapViewModel: AddSpotGoogleMapViewModel
+        
+        init(_ addSpotGoogleMapViewModel: AddSpotGoogleMapViewModel) {
+            self.addSpotGoogleMapViewModel = addSpotGoogleMapViewModel
+        }
+        
+        func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+            print("Current Marker: ", self.addSpotGoogleMapViewModel.selectedMarker)
+            guard let selectedMarker = self.addSpotGoogleMapViewModel.selectedMarker else {
+                // Create new marker
+                let marker = GMSMarker(position: coordinate)
+                marker.map = mapView
+                marker.isDraggable = true
+                self.addSpotGoogleMapViewModel.selectedMarker = marker
+                print("After assignment: " ,self.addSpotGoogleMapViewModel.selectedMarker)
+                return
+            }
+        }
+    }
+}
