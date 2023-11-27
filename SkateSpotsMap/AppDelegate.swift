7f45788370f25e2e789aa33ce9332d140de26ad1
Jason Perez <jasonper@andrew.cmu.edu>
2023-11-27 12:47:55 -0500

    Added code to retrieve API key from file rather than Xcode env

--- SkateSpotsMap/AppDelegate.swift
+++ SkateSpotsMap/AppDelegate.swift
@@ -11,8 +11,14 @@ import FirebaseCore
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool {
-        let apiKey = ProcessInfo.processInfo.environment["MAPS_API_KEY"]
-        GMSServices.provideAPIKey(apiKey!)
+        // Retrieving API key
+        let apiData = Bundle.main.path(forResource: "contents", ofType: "txt")
+        if let apiKey = apiData {
+            print("Successfully retrieved apiKey from file")
+            GMSServices.provideAPIKey(apiKey)
+        } else {
+            print("apiKey was not able to be read from file")
+        }
         
         // Firebase
         FirebaseApp.configure()
