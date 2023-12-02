76bcc535c6ea4ec892b6f461d147adb9ccea6374
Dillon Shu <dillons@cfa323-10.andrew.cmu.edu>
2023-12-01 21:30:31 -0500

    tag experimentation

---
+++ SkateSpotsMap/Views/Filter/TagTestView.swift
@@ -0,0 +1,20 @@
+//
+//  TagTestView.swift
+//  SkateSpotsMap
+//
+//  Created by Dillon Shu on 12/1/23.
+//
+
+import SwiftUI
+
+struct TagTestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TagTestView_Previews: PreviewProvider {
+    static var previews: some View {
+        TagTestView()
+    }
+}
---
+++ SkateSpotsMap/Views/Filter/iOSCheckboxToggleStyle.swift
@@ -0,0 +1,20 @@
+//
+//  iOSCheckboxToggleStyle.swift
+//  SkateSpotsMap
+//
+//  Created by Dillon Shu on 12/1/23.
+//
+
+import SwiftUI
+
+struct iOSCheckboxToggleStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct iOSCheckboxToggleStyle_Previews: PreviewProvider {
+    static var previews: some View {
+        iOSCheckboxToggleStyle()
+    }
+}
