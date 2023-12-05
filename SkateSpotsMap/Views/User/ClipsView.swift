3aeb5b8c27e19555f5d6a61d18768139cc9a8107
Dillon Shu <dillons@cfa323-10.andrew.cmu.edu>
2023-12-05 16:49:17 -0500

    framework for user profile

---
+++ SkateSpotsMap/Views/User/ClipsView.swift
@@ -0,0 +1,20 @@
+//
+//  Clips.swift
+//  SkateSpotsMap
+//
+//  Created by Dillon Shu on 12/5/23.
+//
+
+import SwiftUI
+
+struct ClipsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ClipsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ClipsView()
+    }
+}
---
+++ SkateSpotsMap/Views/User/UserView.swift
@@ -0,0 +1,32 @@
+//
+//  UserView.swift
+//  SkateSpotsMap
+//
+//  Created by Dillon Shu on 12/5/23.
+//
+
+import SwiftUI
+
+struct UserView: View {
+    private var user : User
+    var body: some View {
+        VStack{
+            Text(user.avatar)
+            Text(user.username)
+            Button{
+                
+            }
+            label:{
+                Text("Follow")
+            }
+            ClipsView()
+            SpotsView()
+        }
+    }
+}
+
+//struct UserView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        UserView()
+//    }
+//}
