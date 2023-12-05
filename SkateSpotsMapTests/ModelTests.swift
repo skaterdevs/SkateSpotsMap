8df2d65768b6446ed47a9d22a8779016b6df62b9
leinard0 <darriola@andrew.cmu.edu>
2023-12-05 17:17:14 -0500

    initial testing commit

---
+++ SkateSpotsMapTests/ModelTests.swift
@@ -0,0 +1,18 @@
+//
+//  ModelTests.swift
+//  SkateSpotsMapTests
+//
+//  Created by Daniel on 12/4/23.
+//
+
+import XCTest
+@testable import SkateSpotsMap
+
+final class ModelTests: XCTestCase {
+
+    override func setUp() {
+    }
+
+    override func tearDown() {
+    }
+}
