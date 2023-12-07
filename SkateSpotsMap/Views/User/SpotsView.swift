253603fee16ea120507b3043708d98c4fdbce454
Dillon Shu <dillons@cfa323-10.andrew.cmu.edu>
2023-12-07 09:26:21 -0500

    switching to exp branch

--- SkateSpotsMap/Views/User/SpotsView.swift
+++ SkateSpotsMap/Views/User/SpotsView.swift
@@ -8,13 +8,14 @@
 import SwiftUI
 
 struct SpotsView: View {
+    private var user : User
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct SpotsView_Previews: PreviewProvider {
-    static var previews: some View {
-        SpotsView()
-    }
-}
+//struct SpotsView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SpotsView()
+//    }
+//}
