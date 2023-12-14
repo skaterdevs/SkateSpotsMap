170ff2a82570d76748ad644a4df6acd4e0dd5124
dillons <shudillon@gmail.com>
2023-12-13 22:47:42 -0500

    cleaned up filterviewmodel

--- SkateSpotsMap/ViewModels/FilterViewModel.swift
+++ SkateSpotsMap/ViewModels/FilterViewModel.swift
@@ -10,9 +10,6 @@ import Foundation
 class FilterViewModel: ObservableObject {
     private var skateSpotRepository = SkateSpotRepository()
 
-    @Published var noResults : Bool = false
-
-    //@Published var test : String = ""
     @Published var kickout : String = "N/A"
     @Published var minAvgRating : Int = 1
     @Published var maxDistance : Double = 5.0
@@ -20,8 +17,7 @@ class FilterViewModel: ObservableObject {
                                                        "Banks" : false,
                                                        "Ledges" : false,
                                                        "Curbs" : false]
-//    static let allTags = ["Clean", "New", "Popular", "Dirty", "Safe", "City Areas",
-//                          "Beginner Friendly", "Dangerous"]
+
     @Published var selectedTags : [String:Bool] = ["Clean":false, "New":false, "Popular":false,
                                                    "Dirty":false, "Safe":false, "City Areas":false,
                                                    "Beginner Friendly":false, "Dangerous":false]
@@ -69,33 +65,20 @@ class FilterViewModel: ObservableObject {
             if validFeatures(skateSpot: spot)
                 && validTags(skateSpot: spot)
                 && validRating(skateSpot: spot)
-                //&& validDistance(skateSpot: spot)
                 && validName(searchText:searchText, skateSpot:spot)
                 && validKickout(skateSpot: spot)
                 {
                     spots.append(spot)
                 }
         }
-            
-        
-
         return spots
     }
     
     func validFeatures(skateSpot : SkateSpot) -> Bool{
         let validFeatures : [String] = selectedFeatures.filter{$0.value == true}.map({ $0.key })
         if (skateSpot.features.filter{validFeatures.contains($0)}).count == validFeatures.count || validFeatures.count == 0{
-            //print(validFeatures)
-//            print(skateSpot.name)
-            print("Selected:")
-            print(skateSpot.features)
-            //print(skateSpot.distance)
-            print("")
-            
             return true
         }
-        print("Not Selected:")
-        print(skateSpot.features)
         return false
     }
     
@@ -106,7 +89,7 @@ class FilterViewModel: ObservableObject {
         }
         return false
     }
-    //same behavior as n/a tag; if no reviews it will always be included
+    //If a spot has no reviews it will show up regardless of miniumum review filter
     func validRating(skateSpot : SkateSpot) -> Bool{
         if skateSpot.rating_avg >= Float(minAvgRating) || skateSpot.rating_sum == 0{
             return true
@@ -114,18 +97,8 @@ class FilterViewModel: ObservableObject {
         return false
     }
     
-    //not functioning
-    func validDistance(skateSpot : SkateSpot) async throws -> Bool{
-//        var distanceString = try await SkateSpotViewModel().getDist(
-//            sLat: locationManager.userLatitude(),
-//            sLong: locationManager.userLongitude(),
-//            dLat: skateSpot.location.latitude,
-//            dLong: skateSpot.location.longitude)
-//        print(distanceString)
-        return true
-    
-    }
-    
+
+    //If a spot has no kickout rating it will show up regardless of filter
     func validKickout(skateSpot : SkateSpot) -> Bool {
 
         if skateSpot.overall_kickout == kickout || skateSpot.overall_kickout == Kickout.na.rawValue || kickout == "N/A" {
@@ -142,8 +115,17 @@ class FilterViewModel: ObservableObject {
         }
         return false
     }
-////
     
+    //    func validDistance(skateSpot : SkateSpot) async throws -> Bool{
+    //        var distanceString = try await SkateSpotViewModel().getDist(
+    //            sLat: locationManager.userLatitude(),
+    //            sLong: locationManager.userLongitude(),
+    //            dLat: skateSpot.location.latitude,
+    //            dLong: skateSpot.location.longitude)
+    //        print(distanceString)
+    //        return true
+    //
+    //    }
 
     
 }
